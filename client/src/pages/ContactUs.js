ab5212cd25eb38ea092fbca9c5ddd70a98182987
Zac Smart <zac_chaz@outlook.com>
2023-01-11 16:47:31 +1100

    contact us starter

---
+++ client/src/pages/ContactUs.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function ContactUs() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Name: ", name);
+    console.log("Email: ", email);
+    console.log("Message: ", message);
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </div>
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+
+export default ContactUs;
