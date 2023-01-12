9121078fe1a3ee02dcca163bb85b9c6c43eb9f10
Zac Smart <zac_chaz@outlook.com>
2023-01-12 21:31:17 +1100

    created auth.js for JWT, flushed out typedefs added signup mutation, going to start on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
-const { Tech, Matchup } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Product, Category, Order } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -49,7 +49,11 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(firstName:)
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
   }
 `;
 
